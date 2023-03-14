a9e2ae31b51fdd18ec1b6aeab53e9cce11e2a96f
Graham Spencer <gci.spencer.dev@gmail.com>
2023-03-14 01:36:33 +0000

    Add 2020 data

--- WHR/DataCleanUp.sql
+++ WHR/DataCleanUp.sql
@@ -7,6 +7,7 @@
 
  Version History:
     20220207 - Graham Spencer   - Create initial version
+    20230313 - Graham Spencer   - Add clenup code for year 2020
 
 **********************************************************************************************/
 
@@ -23,6 +24,10 @@ UPDATE WHR.[2017]
 	SET Country = 'Northern Cyprus'
 	WHERE Country = 'North Cyprus';
 
+UPDATE WHR.[2020]
+SET [Country name] = 'Northern Cyprus'
+WHERE [Country name] = 'North Cyprus';
+
 UPDATE WHR.[2015]
 	SET Country = 'Somaliland Region'
 	WHERE Country = 'Somaliland region';
@@ -30,6 +35,10 @@ UPDATE WHR.[2015]
 UPDATE WHR.[2017]
 	SET Country = 'Taiwan'
 	WHERE Country = 'Taiwan Province of China';
+
+UPDATE WHR.[2020]
+SET [Country name] = 'Taiwan'
+WHERE [Country name] = 'Taiwan Province of China';
 
 UPDATE WHR.[2018]
 	SET Country_or_region = 'Trinidad and Tobago'
@@ -43,32 +52,40 @@ UPDATE WHR.[2017]
 	SET Country = 'Hong Kong'
 	WHERE Country = 'Hong Kong S.A.R., China';
 
+UPDATE WHR.[2020]
+SET [Country name] = 'Hong Kong'
+WHERE [Country name] = 'Hong Kong S.A.R. of China';
+
 
 
 -- Insert the list of all the countries from the report into the country list table
 INSERT INTO
-	WHR.country_list (Country)
-	SELECT
-		t.Country
-	FROM (SELECT
-			[Country]
-		FROM [DataAnalysis].[WHR].[2015]
-		UNION
-		SELECT
-			[Country]
-		FROM [DataAnalysis].[WHR].[2016]
-		UNION
-		SELECT
-			[Country]
-		FROM [DataAnalysis].[WHR].[2017]
-		UNION
-		SELECT
-			Country_or_region
-		FROM [DataAnalysis].[WHR].[2018]
-		UNION
-		SELECT
-			Country_or_region
-		FROM [DataAnalysis].[WHR].[2019]) t
+WHR.country_list (Country)
+    SELECT
+        t.Country
+    FROM (SELECT
+            [Country]
+        FROM [DataAnalysis].[WHR].[2015]
+        UNION
+        SELECT
+            [Country]
+        FROM [DataAnalysis].[WHR].[2016]
+        UNION
+        SELECT
+            [Country]
+        FROM [DataAnalysis].[WHR].[2017]
+        UNION
+        SELECT
+            Country_or_region
+        FROM [DataAnalysis].[WHR].[2018]
+        UNION
+        SELECT
+            Country_or_region
+        FROM [DataAnalysis].[WHR].[2019]
+        UNION
+        SELECT
+            [Country name]
+        FROM [DataAnalysis].[WHR].[2020]) t
 
 
 -- update the region column in the country list table from the 2015 and 2016 reports
@@ -95,6 +112,10 @@ UPDATE WHR.country_list
 UPDATE WHR.country_list
 	SET Region = 'Central and Eastern Europe'
 	WHERE Country = 'North Macedonia';
+
+UPDATE WHR.country_list
+SET Region = 'Southern Asia'
+WHERE Country = 'Maldives';
 
 
 -- Insert the scores from each of the year reports into a single scores table
@@ -182,3 +203,20 @@ INSERT INTO
 	FROM WHR.[2019] a
 	INNER JOIN WHR.country_list cl
 		ON a.Country_or_region = cl.Country;
+
+INSERT INTO
+WHR.scores (country_id, Year, Rank, Score, GDP_per_capita, Social_support, Health, Freedom, Trust, Generosity)
+    SELECT
+        cl.country_id
+       ,2020 AS Year
+       ,RANK() OVER (ORDER BY a.[Ladder score] DESC) AS Overall_rank
+       ,a.[Ladder score] AS Score
+       ,a.[Explained by  Log GDP per capita] AS GDP_per_capita
+       ,a.[Explained by  Social support] AS Social_support
+       ,a.[Explained by  Healthy life expectancy] AS Health
+       ,a.[Explained by  Freedom to make life choices] AS Freedom
+       ,a.[Explained by  Perceptions of corruption] AS Trust
+       ,a.[Explained by  Generosity] AS Generosity
+    FROM WHR.[2020] a
+    INNER JOIN WHR.country_list cl
+        ON a.[Country name] = cl.Country;
